1f85f3cf36d570dc908b01e11a6dd82457760253
alexandresys <alexandresys@hotmail.com>
2019-08-28 01:02:05 -0300

    Library Ok

--- test/index.php
+++ test/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, "mail.valesistem.com.br", "adm@valesistem.com.br", "adm1234adm1234", "ssl", "465", "adm@valesistem.com.br", "Alexandresys");
 $novoEmail->sendMail("Assunto teste", "<p>essa é uma Mensagem de <b>teste</b>!</p>", "alexandresys@hotmail.com", "Alexandresys", "alexandresys@hotmail.com", "Alexandre teste");
 
 var_dump($novoEmail);
